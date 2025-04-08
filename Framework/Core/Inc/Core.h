bad1b694f128020ba13c82127fbb89c47fcbf94f
Phi Bao <112589448+PhiBeo@users.noreply.github.com>
2025-04-07 21:54:35 -0700

    BlockAllocator and TypeAllocator

--- Framework/Core/Inc/Core.h
+++ Framework/Core/Inc/Core.h
@@ -2,7 +2,9 @@
 
 #include "Common.h"
 
+#include "BlockAllocator.h"
 #include "DebugUtil.h"
 #include "TimeUtil.h"
 #include "Window.h"
 #include "WindowMessageHandler.h"
+#include "TypeAllocator.h"
